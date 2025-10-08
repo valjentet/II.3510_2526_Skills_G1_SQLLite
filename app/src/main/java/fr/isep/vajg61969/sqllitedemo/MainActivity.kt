ee4a15c3317c027846e900549e74e80fc0d5f5e3
valjentet <49319645+valjentet@users.noreply.github.com>
2025-10-08 15:09:42 +0200

    ,

--- app/src/main/java/fr/isep/vajg61969/sqllitedemo/MainActivity.kt
+++ app/src/main/java/fr/isep/vajg61969/sqllitedemo/MainActivity.kt
@@ -4,57 +4,94 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import fr.isep.vajg61969.sqllitedemo.ui.theme.SQLliteDemoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-         super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             val db = DatabaseHelper(this)
             SQLliteDemoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    androidx.compose.foundation.layout.Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding),
-                        contentAlignment = androidx.compose.ui.Alignment.Center
-                    ) {
-                        androidx.compose.material3.Button(
-                            onClick = {
-                                val id = db.insertTask("Ma première tâche")
-                                println("Tâche insérée avec l'ID $id")
-                            },
-                            modifier = Modifier.padding(innerPadding)
-                        ) {
-                            androidx.compose.material3.Text("Ajouter tâche test")
-                        }
-                    }
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    GreetingScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
+}
 
-        @Composable
-        fun Greeting(name: String, modifier: Modifier = Modifier) {
-            Text(
-                text = "Hello $name!",
-                modifier = modifier
+@Composable
+fun GreetingScreen(modifier: Modifier = Modifier) {
+    var prenom by rememberSaveable { mutableStateOf("") }
+    var submittedName by rememberSaveable { mutableStateOf(false) }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Gray)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        if (submittedName) {
+            OutlinedTextField( //Display a text field
+                value = prenom,
+                onValueChange = { prenom = it },
+                label = { Text("Prénom FDP") },
+                modifier = Modifier.fillMaxWidth()
             )
-        }
-
-        @Preview(showBackground = true)
-        @Composable
-        fun GreetingPreview() {
-            SQLliteDemoTheme {
-                Greeting("Android")
+            Button(
+                onClick = { submittedName = true },
+                //enabled = prenom.isNotBlank()
+            ) {Text("Valider")}
+        } else {
+            Text("Bonjour Valentin")
+            TaskManager(modifier = Modifier.padding(16.dp))
         }
     }
 }
+@Composable
+fun TaskManager(modifier: Modifier = Modifier) {
+    var submittedDisplayTasks by rememberSaveable { mutableStateOf(false) }
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.LightGray)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { submittedDisplayTasks = true }
+        ) {}
+    }
+}
+
+
+
+
+
+
+
